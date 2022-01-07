6baaf6e5cf27d491eeccc6f1484fba6d58cff1af
Luis Machillanda <nikko1801@gmail.com>
2022-01-06 20:44:37 -0500

    fixing child routes

--- src/components/unauthenticatedHeader/index.tsx
+++ src/components/unauthenticatedHeader/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { Button } from '../shared/button';
 import { Header } from '../shared/header';
 import { TYPES } from '../shared/types';
@@ -9,24 +9,27 @@ import { default as logo } from '../../assets/logoV1.svg';
 export const UnauthenticatedHeader = () => {
 	const navigate = useNavigate();
     return (
-		<Header height='130px'>
-			<Wrapper>
-				<Link to="/">
-					<Wrapper height="30px">
-						<img src={logo} />
-					</Wrapper>
-				</Link>
-			</Wrapper>
-			<Wrapper display="flex" justifyContent="end">
-				<Button
-					onClick={() => navigate('/login')}
-					width="150px"
-					letterSpacing="1px"
-					fontSize="sm"
-					type={TYPES.PRIMARY}
-					text="Login"
-				/>
-			</Wrapper>
-		</Header>
+		<>
+			<Header height='130px'>
+				<Wrapper>
+					<Link to="/">
+						<Wrapper height="30px">
+							<img src={logo} />
+						</Wrapper>
+					</Link>
+				</Wrapper>
+				<Wrapper display="flex" justifyContent="end">
+					<Button
+						onClick={() => navigate('/login')}
+						width="150px"
+						letterSpacing="1px"
+						fontSize="sm"
+						type={TYPES.PRIMARY}
+						text="Login"
+					/>
+				</Wrapper>
+			</Header>
+            <Outlet />
+		</>
     )
 }
